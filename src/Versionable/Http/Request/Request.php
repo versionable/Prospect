f5d8933f3200ae0b254b71a9b25eb8ee291520ab
Stuart Lowes <stuart.lowes@gmail.com>
2010-12-07 00:06:14 +0000

    Request will now return the full http request when toString is called

--- src/Versionable/Http/Request/Request.php
+++ src/Versionable/Http/Request/Request.php
@@ -6,10 +6,13 @@ use Versionable\Http\Url\UrlInterface;
 use Versionable\Http\Cookie\CollectionInterface as CookieCollectionInterface;
 use Versionable\Http\Header\CollectionInterface as HeaderCollectionInterface;
 use Versionable\Http\Parameter\CollectionInterface as ParameterCollectionInterface;
+use Versionable\Http\File\CollectionInterface as FileCollectionInterface;
 
 class Request implements RequestInterface
 {
   protected $url = null;
+  
+  protected $port = null;
 
   protected $method = 'GET';
 
@@ -19,15 +22,28 @@ class Request implements RequestInterface
 
   protected $parameters = null;
   
-  protected $agent = null;
+  protected $files = null;
+  
+  protected $body = '';
+
+  protected $version = 1.1;
 
   public function setUrl(UrlInterface $url) {
     $this->url = $url;
   }
+  
   public function getUrl() {
     return $this->url;
   }
+  
+  public function getBody() {
+    return $this->body;
+  }
 
+  public function setBody($body) {
+    $this->body = $body;
+  }
+  
   public function setParameters(ParameterCollectionInterface $parameters) {
     $this->parameters = $parameters;
   }
@@ -40,6 +56,18 @@ class Request implements RequestInterface
     return !\is_null($this->parameters);
   }
 
+  public function setFiles(FileCollectionInterface $files) {
+    $this->files = $files;
+  }
+
+  public function getFiles() {
+    return $this->files;
+  }
+  
+  public function hasFiles() {
+    return !\is_null($this->files);
+  }
+  
   public function getMethod() {
     return $this->method;
   }
@@ -78,5 +106,71 @@ class Request implements RequestInterface
     }
 
     return false;
+  }
+  
+  public function getPort() {
+    
+    if (is_numeric($this->port) && !\is_null($this->url)) {
+      return $this->getUrl()->getPort();
+    }
+      
+    return 80;
+  }
+
+  public function setPort($port) {
+    $this->port = $port;
+  }
+  
+  public function getVersion() {
+    return $this->version;
+  }
+
+  public function setVersion($version) {
+    $this->version = $version;
+  }
+    
+  public function toString() {
+    
+    $data = "";
+    if (!is_null($this->url)) {
+      $data = \sprintf("%s %s HTTP/%s\r\n", $this->getMethod(), $this->url->getPathAndQuery(), $this->getVersion());
+      $data .= \sprintf("Host: %s\r\n", $this->url->getHostname());
+    }
+    
+    if ($this->hasHeaders()) {
+      foreach ($this->getHeaders() as $header) {
+        $data .= $header . "\r\n";
+      }
+    }
+   
+    
+    
+    $body = '';
+    if ($this->hasParameters() || $this->getBody() != '') {
+      $data .= "Content-Type: application/x-www-form-urlencoded\r\n";
+
+      if ($this->hasParameters()) {
+        $body = $this->getParameters();
+      }
+      elseif ($this->getBody() != '') {
+        
+        $body = $this->getBody();
+      }
+      $data .= "Content-Length: ".strlen($body)."\r\n";
+    }
+    
+    if (\strlen($body)) {
+      $data .= "\r\n". $body;
+    }
+    
+    if ($this->hasFiles()) {
+      $data .= $this->getFiles();
+    }
+     
+    return $data;
+  }
+  
+  public function __toString() {
+    return $this->toString();
   }
 }
--- src/Versionable/Http/Request/RequestInterface.php
+++ src/Versionable/Http/Request/RequestInterface.php
@@ -6,17 +6,28 @@ use Versionable\Http\Url\UrlInterface;
 use Versionable\Http\Cookie\CollectionInterface as CookieCollectionInterface;
 use Versionable\Http\Header\CollectionInterface as HeaderCollectionInterface;
 use Versionable\Http\Parameter\CollectionInterface as ParameterCollectionInterface;
+use Versionable\Http\File\CollectionInterface as FileCollectionInterface;
 
 interface RequestInterface {
   public function setUrl(UrlInterface $url);
   
   public function getUrl();
+  
+  public function setBody($body);
+  
+  public function getBody();
 
   public function setParameters(ParameterCollectionInterface $parameters);
 
   public function getParameters();
   
   public function hasParameters();
+  
+  public function setFiles(FileCollectionInterface $files);
+
+  public function getFiles();
+  
+  public function hasFiles();
 
   public function setMethod($method);
 
@@ -33,4 +44,12 @@ interface RequestInterface {
   public function getCookies();
   
   public function setCookies(CookieCollectionInterface $collection);
+  
+  public function setPort($port);
+  
+  public function getPort();
+  
+  public function setVersion($version);
+  
+  public function getVersion();
 }
