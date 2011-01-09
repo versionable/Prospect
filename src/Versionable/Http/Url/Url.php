98435e1943822f139cc9d505902b31ca55f13701
Stuart Lowes <stuart.lowes@gmail.com>
2011-01-09 11:05:31 +0000

    Modifying getters for boolean properties

--- src/Versionable/Http/Url/Url.php
+++ src/Versionable/Http/Url/Url.php
@@ -3,77 +3,92 @@
 namespace Versionable\Http\Url;
 
 class Url implements UrlInterface {
-  
+
   protected $scheme = 'http';
-  
+
   protected $hostname = 'localhost';
-  
+
   protected $port = 80;
-  
+
   protected $username = null;
-  
+
   protected $password = null;
-  
+
   protected $path = '/';
 
-  protected $parameters = array();  
-  
+  protected $parameters = array();
+
   protected $fragment = '';
-  
-  protected $url = '';
-  
+
+  protected $query_separator = '&';
+
+
   public function __construct($url, $parameters = array()) {
     $this->setParameters($parameters);
     $this->setUrl($url);
   }
-  
+
   public function __toString() {
     return $this->toString();
   }
-  
+
   public function toString() {
     return $this->getUrl();
   }
 
+  /**
+   *
+   * TODO: Return assembled string from other properties
+   *
+   * @return string
+   */
   public function getUrl() {
-    return $this->url;
+    return $this->buildUrl();
   }
 
   public function setUrl($url) {
-    
+
     if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED) === false) {
       throw new \RuntimeException('Not a valid Url');
     }
-    
+
     $components = parse_url($url);
     if (is_array($components))
-    {      
+    {
       if (isset($components['user'])) {
         $this->setUsername($components['user']);
         if (isset($components['pass'])) {
           $this->setPassword($components['pass']);
         }
       }
-    
+
       $this->setScheme($components['scheme']);
-      
-      if (\is_array($parameters)) {
-        $this->setParameters(array_merge($this->parameters, parse_str($components['parameters'], $parameters)));
+
+      if (! empty($components['query'])) {
+        $this->setParameters(array_merge($this->parameters, explode($this->query_separator, $components['query'])));
       }
       $this->setHostname($components['host']);
-      $this->setPath($components['path']);
-      $this->setPort($components['port']);
-      $this->setScheme($components['scheme']);
-      $this->setFragment($components['fragment']);
-    }
 
-    $this->url = $url;
+      if (isset($components['path'])) {
+        $this->setPath($components['path']);
+      }
+
+      if (isset($components['port'])) {
+        $this->setPort($components['port']);
+      }
+
+      $this->setScheme($components['scheme']);
+
+      if (isset($components['fragment'])) {
+        $this->setFragment($components['fragment']);
+      }
+    }
   }
-  
+
   public function setParameters(array $parameters) {
     $this->parameters = $parameters;
   }
-  
+
   public function getParameters() {
     return $this->parameters;
   }
@@ -94,7 +109,7 @@ class Url implements UrlInterface {
   public function hasParameter($name) {
     return isset($this->parameters[$name]);
   }
-  
+
   public function setHostname($hostname) {
     $this->hostname = $hostname;
   }
@@ -102,64 +117,63 @@ class Url implements UrlInterface {
   public function getHostname() {
     return $this->hostname;
   }
-  
+
   public function setScheme($scheme) {
-    
+
     if ($this->getPort() == 80 || $this->getPort() == 443) {
-      if ($this->getScheme() == 'http') {
+      if ($scheme == 'http') {
         $this->setPort(80);
-      } elseif ($this->getScheme() == 'https') {
+      } elseif ($scheme == 'https') {
         $this->setPort(443);
       }
     }
-    
+
     $this->scheme = $scheme;
   }
 
   public function getScheme() {
     return $this->scheme;
   }
-  
+
   public function setPort($port) {
     if (\is_numeric($port)) {
       $this->port = $port;
     }
   }
 
-  public function getPort() { 
-    var_dump($this);
+  public function getPort() {
     return $this->port;
   }
-  
+
   public function setUsername($username) {
     $this->username = $username;
   }
 
   public function getUsername() {
-    return $this->use;
+    return $this->username;
   }
-  
+
   public function setPassword($password) {
     $this->password = $password;
   }
 
   public function getPassword() {
-    return \parse_url($this->url, \PHP_URL_PASS);
+    return $this->password;
   }
-  
+
   public function setPath($path) {
-    
+
     if(empty($path)) {
       $path = '/';
     }
-    
+
     $this->path = $path;
   }
 
   public function getPath() {
     return $this->path;
   }
-  
+
   public function setFragment($fragment) {
     $this->fragment = $fragment;
   }
@@ -167,14 +181,61 @@ class Url implements UrlInterface {
   public function getFragment() {
     return $this->fragment;
   }
-  
+
   public function getPathAndQuery() {
-    
-    $path = $this->path;
-    if (count($this->parameters) > 0) {
-      $path .= "?" . http_build_query($this->getParameters());
-    }
-    
+
+    $path = $this->path . $this->getQuery();
+
     return $path;
+  }
+
+  public function getQuery() {
+    $query = '';
+    if (count($this->parameters) > 0) {
+      $query = '?' . http_build_query($this->getParameters());
+    }
+
+    return $query;
+  }
+
+  protected function buildUrl() {
+    $components = array(
+      'scheme' => $this->getScheme(),
+      'host' => $this->getHostname(),
+      'port' => $this->getPort(),
+      'user' => $this->getUsername(),
+      'pass' => $this->getPassword(),
+      'path' => $this->getPath(),
+      'query' => $this->getQuery(),
+      'fragment' => $this->getFragment()
+    );
+
+    if (!function_exists('http_build_url'))
+    {
+      $keys = array('user','pass','port','path','query','fragment');
+
+      foreach ($keys as $key)
+      {
+        if (empty($components[$key])) {
+          unset($components[$key]);
+        }
+      }
+
+      if ($components['port'] == 80) {
+        unset($components['port']);
+      }
+
+			return
+				 ((isset($components['scheme'])) ? $components['scheme'] . '://' : '')
+				.((isset($components['user'])) ? $components['user'] . ((isset($components['pass'])) ? ':' . $components['pass'] : '') .'@' : '')
+				.((isset($components['host'])) ? $components['host'] : '')
+				.((isset($components['port'])) ? ':' . $components['port'] : '')
+				.((isset($components['path'])) ? $components['path'] : '')
+				.((isset($components['query'])) ? $components['query'] : '')
+				.((isset($components['fragment'])) ? '#' . $components['fragment'] : '');
+    }
+
+    return http_build_url($components);
+
   }
 }
