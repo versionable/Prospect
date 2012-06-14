2355d6b79a44235014c33b5dc820d92a0f4cc16d
Harry Walter <harry.walter@lqdinternet.com>
2012-06-14 14:57:05 +0100

    Use composer for autoload and test dependancies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,18 @@
 <?php
 
-require_once(__DIR__.'/../vendor/autoload/src/Versionable/Autoload/Autoload.php');
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
 
-use Versionable\Autoload\Autoload;
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install --dev'.PHP_EOL);
+}
 
-$al = new Autoload();
-$al->registerNamespace('Versionable\Tests\Prospect', __DIR__.'/../tests');
-$al->registerNamespace('Versionable\Prospect', __DIR__.'/../src');
-$al->registerNamespace('Versionable\Common', __DIR__.'/../vendor/versionable-common/src');
-$al->register();
+$loader->add('Versionable\Tests\Prospect', __DIR__);
+$loader->add('Versionable\Prospect', __DIR__.'/../src');
+$loader->add('Versionable\Common', __DIR__.'/../vendor/versionable-common/src');
